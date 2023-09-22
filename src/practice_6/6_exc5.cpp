5423c390dbcad6339feba11421d2d36233b17026
rockwatch <olivera9806@gmail.com>
2023-09-22 02:34:29 -0400

    finish file reading function into patients array

--- src/practice_6/6_exc5.cpp
+++ src/practice_6/6_exc5.cpp
@@ -14,22 +14,107 @@ A medical clinic diagnoses possible diseases that its patients may be suffering 
 using namespace std;
 
 struct Disease {
-    string symptom1;
-    string symptom2;
-    string symptom3;
-    string symptom4;
-};
+    string disease_name;
+    string symptoms[4];
+    int countM;
+    int countF;
+    int countT = countM + countF;
+}; // structure for Disease
 
 struct Patient {
     string name;
     bool gender;
     int age;
-    Disease arr[4];
-}; 
+    string sympt[4];
+}; // structure for Patient
+
+void fillArray(Patient arr[]){
+    int i = 0;
+    while(i < 10){
+        Patient p;
+
+        p.name = "";
+        p.gender = false;
+        p.age = 0;
+        int j = 0;
+        while(j < 4){
+            p.sympt[j] = "";
+            j++;
+        }
+
+        arr[i] = p;
+
+        i++;
+    }
+}
+// fill array function 
+
+void printPatient(Patient arr[10]){
+    int i = 0;
+    while( i < 10){
+        cout << "Patient's name: " << arr[i].name << endl;
+        cout << "gender: " << arr[i].gender << endl;
+        cout << "age: " << arr[i].age << endl;
+
+        int k = 0;
+        while(k < 4){
+            cout << "Patient's symptom N°" << k + 1 << ": " << arr[i].sympt[k] << endl;
+            k++;
+        }                                
+        cout << endl;
+        i++;
+    }
+}
+// print patient function
 
 int main(){
 
-    /*TODO DO FUNCTION TO READ FILES AND FILL AN ARRAY*/
+    
+    Patient patients[10]; // creating patients array
+    fillArray(patients); //filling patients array with dummy data
+    printPatient(patients); // printing patients array in console
+    //filling and printing patients list for the first time
+    
+    fstream inputfile;
+
+    inputfile.open("patients.txt", ios::in);
+
+    if(inputfile.is_open()){
+        cout << "It's open" << endl;
+
+        string name;
+        bool gender;
+        int age;
+        string symptom;
+
+        int i = 0;
+
+        while(i < 10){
+
+            inputfile >> name;
+            patients[i].name = (string)name;
+            
+
+            inputfile >> gender;
+            patients[i].gender = (bool)gender;
+ 
+            inputfile >> age;
+            patients[i].age = (int)age;
+
+            int j = 0;
+            while(j < 4){
+                inputfile >> symptom;
+                patients[i].sympt[j] = (string)symptom;
+                j++;
+            }
+
+            i++;
+
+        }
+    }
+    inputfile.close();
+    
+    printPatient(patients);
 
     return 0;
 }
